cbc82fc2036ee23a85e5481d8cccd9cd9e06baa6
Nikolay Markin <hawk1409@yandex.ru>
2025-03-01 11:56:17 +0700

    Unit 9.3

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+# Импортируем функцию reverse() для получения ссылки на объект.
+from django.urls import reverse
 # Импортируется функция-валидатор.
 from .validators import real_age
 
@@ -18,3 +20,6 @@ class Birthday(models.Model):
                 name='Unique person constraint',
             ),
         )
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
