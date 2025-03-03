a8e074f5da61f40c3c0e1b95619c873b9090a804
Nikolay Markin <hawk1409@yandex.ru>
2025-03-03 19:21:13 +0700

    Unit 10/2/2

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -2,7 +2,7 @@ from django import forms
 # Импортируем класс ошибки валидации.
 from django.core.exceptions import ValidationError
 # Импортируем класс модели Birthday.
-from .models import Birthday
+from .models import Birthday, Congratulation
 # Импорт функции для отправки почты.
 from django.core.mail import send_mail
 
@@ -49,4 +49,11 @@ class BirthdayForm(forms.ModelForm):
             )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
-            ) 
+            )
+
+
+class CongratulationForm(forms.ModelForm):
+
+    class Meta:
+        model = Congratulation
+        fields = ('text',)
